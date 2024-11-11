db3afaa432f5d86bf6a4574d4def3ec65d6d3b9d
Rajneesh9034 <rajneshk2017@gmail.com>
2024-11-11 15:28:29 +0530

    hi

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::post('/WithdrawRequest', [App\Http\Controllers\UserPanel\WithdrawRequest:
 Route::get('/transactionHistory', [App\Http\Controllers\UserPanel\AddFund::class, 'index']);
 Route::post('/edit-password', [App\Http\Controllers\UserPanel\Profile::class, 'change_password_post']);
 Route::any('/forgot_submit', [App\Http\Controllers\Login::class, 'forgot_password_submit']);
+Route::get('/levelTeam', [App\Http\Controllers\UserPanel\Team::class, 'LevelTeam']);
 
 Route::middleware(['auth:sanctum', 'check.app.key'])->get('/protected-data', function (Request $request) {
         return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/clear', function() {
+
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+ 
+    return "Cleared!";
+ 
+ });
+ 
 
 Auth::routes();
 
