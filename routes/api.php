10396b739f2bb44d42cf56b412969d572c264960
SAHILDEWAN23 <sahildewan2323@gmail.com>
2024-11-12 13:38:59 +0530

    ok

--- routes/api.php
+++ routes/api.php
@@ -28,10 +28,10 @@ Route::middleware(['auth:sanctum', 'check.app.key'])->group(function () {
     Route::get('/levelTeam', [App\Http\Controllers\UserPanel\Team::class, 'LevelTeam']);
     Route::get('/transactionHistory', [App\Http\Controllers\UserPanel\AddFund::class, 'index']);
     Route::any('/confirmDeposit', [App\Http\Controllers\UserPanel\Invest::class, 'confirmDeposit']);
-Route::post('/WithdrawRequest', [App\Http\Controllers\UserPanel\WithdrawRequest::class, 'WithdrawRequest']);
-Route::post('/edit-password', [App\Http\Controllers\UserPanel\Profile::class, 'change_password_post']);
-Route::get( '/levelTeam', [App\Http\Controllers\UserPanel\Team::class, 'LevelTeam']);
-Route::post('/Step', [App\Http\Controllers\UserPanel\stepCount::class, 'step_count']);
+    Route::post('/WithdrawRequest', [App\Http\Controllers\UserPanel\WithdrawRequest::class, 'WithdrawRequest']);
+    Route::post('/edit-password', [App\Http\Controllers\UserPanel\Profile::class, 'change_password_post']);
+    Route::get( '/levelTeam', [App\Http\Controllers\UserPanel\Team::class, 'LevelTeam']);
+    Route::post('/Step', [App\Http\Controllers\UserPanel\stepCount::class, 'step_count']);
 
 });
 
