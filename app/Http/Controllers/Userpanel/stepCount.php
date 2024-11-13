ffb5b3daae8a839b67c8ff6f18655675d6c9bd76
SAHILDEWAN23 <sahildewan2323@gmail.com>
2024-11-13 16:08:00 +0530

    ok

--- app/Http/Controllers/Userpanel/stepCount.php
+++ app/Http/Controllers/Userpanel/stepCount.php
@@ -9,7 +9,6 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
-use App\Models\Investment;
 use App\Models\User;
 use Redirect;
 use Helper;
@@ -24,6 +23,7 @@ class stepCount extends Controller
         $user=Auth::user();
          $validation =  Validator::make($request->all(), [
                 'step' => 'required',
+
             ]);
             if($validation->fails()) {
 
@@ -73,7 +73,7 @@ class stepCount extends Controller
                 
                 'message' => 'data save Successfully.' // Returns all error messages
             ], 200);}
-        }
+    }
        
 
         public function step_history(){
@@ -92,16 +92,6 @@ class stepCount extends Controller
                     ], 200);
                
                
-        }
-
-        public function checkstep_bonus(){
-            $user=Auth::user();
-            $get_step=UserStep::where('user_id',1)->whereDate('today', now()->format('Y-m-d'))
-            ->value('step');
-           
-          if($get_step>=500){
-            $get_invest=Investment::where('user_id',1)->('status','Active')->
-          }
         }
 
 }
