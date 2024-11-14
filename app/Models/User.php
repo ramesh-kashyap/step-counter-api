a902349268bfc980a55338563d1bdf28f0f4160e
Rajneesh9034 <rajneshk2017@gmail.com>
2024-11-14 15:03:57 +0530

    hii

--- app/Models/User.php
+++ app/Models/User.php
@@ -143,11 +143,7 @@ class User extends Authenticatable
     
     
     
-    public function available_balance()
-    {
-    $balance = ($this->totalInvestmentSum()+Auth::user()->users_incomes()) - ($this->totalWithdrawalSum());
-    return $balance;
-    } 
+   
 
     public function principleBalance()
     {
@@ -212,7 +208,11 @@ class User extends Authenticatable
                     ->where('status', 'Approved') // Filters by today's date
                     ->sum('amount'); // Sum the 'amount' of today's withdrawals
     }
-
+    public function available_balance()
+    {
+    $balance = ($this->totalInvestmentSum()+Auth::user()->users_incomes()) - ($this->totalWithdrawalSum());
+    return $balance;
+    } 
   public function Priciplewithdrawal(){
         return $this->hasMany('App\Models\Withdraw','user_id','id')->where('walletType',2);
     }
