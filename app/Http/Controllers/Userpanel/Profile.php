facdcdfdfcc49eb8969ca08cf4d61d790e063468
SAHILDEWAN23 <sahildewan2323@gmail.com>
2024-11-14 15:27:32 +0530

    ok

--- app/Http/Controllers/Userpanel/Profile.php
+++ app/Http/Controllers/Userpanel/Profile.php
@@ -66,7 +66,6 @@ class Profile extends Controller
     $todayTeamincome=Income::Where('user_id',$user->id)->where('remarks','Team Income')->whereDate('ttime',$today)->sum('comm');
     $totalsteps=UserStep::Where('user_id',$user->id)->sum('step');
     $todaysteps=UserStep::Where('user_id',$user->id)->whereDate('today',$today)->sum('step');
-    $available_balance=$user->available_balance();
 
     $available_balance = ($user->totalInvestmentSum() + $user->users_incomes()) - ($user->totalWithdrawalSum());
     $wallets =[
